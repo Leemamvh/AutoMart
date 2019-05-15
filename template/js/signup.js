81b063b9d47d1878d148d2142921102b07130442
leemamvh <39867489+Leemamvh@users.noreply.github.com>
2019-05-15 11:24:32 +0100

    feat(signup-page): a user should see a sign up page to register -add signup form -add signin form -add js to toggle betwwen both forms [Finishes #166001116]

---
+++ template/js/signup.js
@@ -0,0 +1,17 @@
+const myFunctions = () => {
+    
+    document.getElementById("signin-info").addEventListener("click", (e) => {
+    //hide  the signup form
+    document.getElementById("sign-up").style.display = 'none'
+        //show signin form  
+        document.getElementById("sign-in").style.display = ''
+    })
+        document.getElementById("signup-info").addEventListener("click", (e) => {
+            
+            document.getElementById("sign-in").style.display = 'none'
+                 
+                document.getElementById("sign-up").style.display = ''
+    })
+
+}  
+ 
