15b0f87a60482d632dca6296f3ef4307fcea27c5
leemamvh <39867489+Leemamvh@users.noreply.github.com>
2019-05-15 11:24:32 +0100

    feat(signup-page): a user should see a sign up page to register -add signup form -add signin form -add js to toggle betwwen both forms [Finishes #166001116]

---
+++ template/js/signup.js
@@ -0,0 +1,8 @@
+const notice = document.getElementById("notice")
+if (notice) {
+    console.log('%%%%%%%%%')
+   notice.addEventListener("click", () => {
+        console.log('sign in')
+    })
+
+}
